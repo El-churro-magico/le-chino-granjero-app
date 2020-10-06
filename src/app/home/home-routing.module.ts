d415fdb384254f82a667e49008db7d46863e5afd
kevv87 <magneticok@gmail.com>
2020-10-06 16:19:08 -0600

    Adding the profile page

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home.page';
+import { ProfilePage } from './profile/profile.page';
 
 const routes: Routes = [
   {
     path: '',
     component: HomePage,
+  },
+  {
+    path: 'tienda',
+    redirectTo: ''
+  },
+  {
+    path: 'profile',
+    component: ProfilePage
   }
 ];
 
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
+
 import { HomePage } from './home.page';
+import { ProfilePage } from './profile/profile.page'
 
 import { HomePageRoutingModule } from './home-routing.module';
 import { ProductorCardComponent } from '../components/productor-card/productor-card.component';
 import { HeaderComponent } from '../components/header/header.component';
 import {FooterComponent} from '../components/footer/footer.component';
+
 
 @NgModule({
   imports: [
@@ -18,6 +21,7 @@ import {FooterComponent} from '../components/footer/footer.component';
   ],
   declarations: [
     HomePage,
+    ProfilePage,
     ProductorCardComponent,
     HeaderComponent,
     FooterComponent
